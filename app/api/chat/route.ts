492c960033f0776fb50663c745157b82277f2ca8
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2025-03-05 20:00:14 +0100

    feat: install vercel ai sdk

---
+++ app/api/chat/route.ts
@@ -0,0 +1,16 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: openai('gpt-4o-mini-2024-07-18'),
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
