389f89e850b9db28db67e334daeb6a92d6971816
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2025-03-19 21:53:28 +0100

    chore: remove sidebar

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,19 +1,11 @@
 import '@/app/ui/global.css';
-import { AppSidebar } from '@/components/ui/app-sidebar';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { ThemeProvider } from '@/components/ui/theme/theme-provider';
 import { ReactNode } from 'react';
 
 export default function HomeLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-            <SidebarProvider>
-                <AppSidebar />
-                <main>
-                    <SidebarTrigger />
-                    {children}
-                </main>
-            </SidebarProvider>
+            <main>{children}</main>
         </ThemeProvider>
     );
 }
